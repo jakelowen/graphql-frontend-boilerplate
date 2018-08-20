bffdc0e99cd97690094ee0a8cafe7083abed5854
Jake Lowen <jakelowen@gmail.com>
2018-08-19 20:11:27 -0500

    swapping url back to env

--- src/apollo.ts
+++ src/apollo.ts
@@ -22,7 +22,7 @@ const errorLink = onError(({ networkError, graphQLErrors }) => {
 
 const httpLink = new HttpLink({
   credentials: "include",
-  uri: "https://vast-depths-13746.herokuapp.com/"
+  uri: process.env.REACT_APP_SERVER_URL
 });
 
 const authLink = setContext((_, { headers }) => {
